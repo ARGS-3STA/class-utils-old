13975146beea3996bc00732bc901452259adbd3f
DanielGrotan <78237277+DanielGrotan@users.noreply.github.com>
2022-03-28 19:15:38 +0200

    Create data_loader

---
+++ algorithm/data_loader.py
@@ -0,0 +1,6 @@
+def load_class_list(file_path: str):
+    try:
+        with open(file_path, "r", encoding="latin-1") as class_list:
+            return class_list.read().splitlines()
+    except FileNotFoundError:
+        print("Can't find file with path:", file_path)
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import os
+
+from algorithm.data_loader import load_class_list
 from application import App
 
 
 def main() -> None:
-    app = App(800, 600, "Test")
-    app.run()
+    # app = App(800, 600, "Test")
+    # app.run()
+
+    BASE_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+    ASSETS_DIRECTORY = os.path.join(BASE_DIRECTORY, "assets")
+
+    load_class_list(os.path.join(ASSETS_DIRECTORY, "klasseliste.txt"))
 
 
 if __name__ == "__main__":
