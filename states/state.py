a0636186b39d281065c83904f7e56a69e0275038
DanielGrotan <78237277+DanielGrotan@users.noreply.github.com>
2022-03-28 18:34:36 +0200

    added more type hints

--- states/state.py
+++ states/state.py
@@ -1,13 +1,13 @@
-from typing import Any
-
+from application import App
 from pygame import Surface
+from pygame.event import Event
 
 
 class State:
-    def __init__(self, app):
+    def __init__(self, app: App):
         self.app = app
 
-    def update(self, actions: list[Any], deltatime: float) -> None:
+    def update(self, events: list[Event], deltatime: float) -> None:
         raise NotImplementedError()
 
     def draw(self, window: Surface) -> None:
