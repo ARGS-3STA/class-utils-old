e9f412dab268c38d2aad72c1768c118f159ed843
sheeshDotExe <viktorvikingstad0312@gmail.com>
2022-03-28 22:02:52 +0200

    Comment

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from application import App
 
 
 def main() -> None:
+    # main
     BASE_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
     ASSETS_DIRECTORY = os.path.join(BASE_DIRECTORY, "assets")
 
