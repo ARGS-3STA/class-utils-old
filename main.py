578ff9952185023de30d85af1d38341946167514
DanielGrotan <78237277+DanielGrotan@users.noreply.github.com>
2022-03-30 20:32:40 +0200

    pycache moment

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ def main() -> None:
     BASE_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
     ASSETS_DIRECTORY = os.path.join(BASE_DIRECTORY, "assets")
 
-    # seating_plan_generator = SeatingPlanGenerator(ASSETS_DIRECTORY)
+    seating_plan_generator = SeatingPlanGenerator(ASSETS_DIRECTORY)
 
-    # print(seating_plan_generator.generate("1stn", "Rom 205"))
+    print(seating_plan_generator.generate("1stn", "Rom 205"))
 
     # groups = GroupMaker(os.path.join(ASSETS_DIRECTORY, "klasseliste.txt"))
     # print(groups.groups_from_amounts_of_groups(10))
